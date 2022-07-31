d205836279e2c0cb4228b2f49703f0e4b2b21786
Laurence Davies <ljd.davies@gmail.com>
2022-07-31 12:58:54 +0200

    first commit

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,44 +3,48 @@
 pragma solidity ^0.8.13;
 import "hardhat/console.sol";
 
+/**
+ @title Lottery
+ */
 contract Lottery {
     // declaring the state variables
     address[] public players; //dynamic array of type address payable
     address[] public gameWinners;
     address public owner;
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
+    }
+
     // declaring the constructor
     constructor() {
-        // TODO: initialize the owner to the address that deploys the contract
+        owner = msg.sender;
     }
 
     // declaring the receive() function that is necessary to receive ETH
     receive() external payable {
-        // TODO: require each player to send exactly 0.1 ETH
-        // TODO: append the new player to the players array
+        require(msg.value == 0.1 ether, "INSUFFICIENT_VALUE");
+        players.push(msg.sender);
     }
 
     // returning the contract's balance in wei
-    function getBalance() public view returns (uint256) {
-        // TODO: restrict this function so only the owner is allowed to call it
-        // TODO: return the balance of this address
+    function getBalance() public view onlyOwner returns (uint256) {
+        return address(this).balance;
     }
 
     // selecting the winner
-    function pickWinner() public {
-        // TODO: only the owner can pick a winner 
-        // TODO: owner can only pick a winner if there are at least 3 players in the lottery
+    function pickWinner() public onlyOwner {
+        require(players.length >= 3, "NOT_ENOUGH_PLAYERS");
 
-        uint256 r = random();
-        address winner;
+        uint256 randomIndex = random() % players.length;
+        address winner = players[randomIndex];
 
-        // TODO: compute an unsafe random index of the array and assign it to the winner variable 
+        gameWinners.push(winner);
+        players = new address[](0);
 
-        // TODO: append the winner to the gameWinners array
-
-        // TODO: reset the lottery for the next round
-
-        // TODO: transfer the entire contract's balance to the winner
+        (bool success, ) = winner.call{value: getBalance()}("");
+        require(success, "TRANSFER_FAILED");
     }
 
     // helper function that returns a big random integer
